52fd0a4b7a7caed06b3018e0c8c88bdc85f195e6
Darian Campos <darian.campos@pedidosya.com>
2020-01-13 14:13:14 -0300

    create nuevo branch

--- src/api/server.go
+++ src/api/server.go
@@ -8,36 +8,10 @@ import (
 )
 
 func Run() {
+	fmt.Println("Hola soy un comnetario nuevo")
 	fmt.Println("\n\tServer listening[::]:3000")
-
-
 	r := router.New()
 	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-
 	log.Fatal(http.ListenAndServe(":3000", r))
 
 }
