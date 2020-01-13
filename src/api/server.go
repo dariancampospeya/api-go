7077d56bfac819f1e7004f079b8f59303ca87e3c
Darian Campos <darian.campos@pedidosya.com>
2020-01-13 14:26:51 -0300

    refactor class user.go

--- src/api/server.go
+++ src/api/server.go
@@ -9,35 +9,6 @@ import (
 
 func Run() {
 	fmt.Println("\n\tServer listening[::]:3000")
-
-
 	r := router.New()
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-	fmt.Println("\n\tServer listening[::]:3000")
-
 	log.Fatal(http.ListenAndServe(":3000", r))
-
 }
