66dc7dbe4603061aa59fdb75e46f9aeda591904f
Darian Campos <darian.campos@pedidosya.com>
2020-01-13 12:40:41 -0300

    delete line server

--- src/api/server.go
+++ src/api/server.go
@@ -10,14 +10,6 @@ import (
 func Run() {
 	fmt.Println("\n\tServer listening[::]:3000")
 
-	fmt.Println("\n\tServer listening[::]:3000")
-
-	fmt.Println("\n\tServer listening[::]:3000")
-
-	fmt.Println("\n\tServer listening[::]:3000")
-
-	fmt.Println("\n\tServer listening[::]:3000")
-
 	r := router.New()
 	log.Fatal(http.ListenAndServe(":3000", r))
 }
